e7177587aeafdad6a996797131272cd425a2a99f
lukasz-ruszkiewicz <karr190@wp.pl>
2020-09-01 11:15:40 +0200

    Added full editor functionality and changed text input to textarea

--- src/js/index.js
+++ src/js/index.js
@@ -8,22 +8,25 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀')
 
-let savedText = "";
 
 const textArea = document.querySelector('.editor-form__input');
 const buttonLoad = document.querySelector('.editor-form__button--load');
 const buttonSave = document.querySelector('.editor-form__button--save');
+//textArea.value = savedText;
 
 buttonLoad.addEventListener('click', loadText);
 buttonSave.addEventListener('click', saveText);
 
+
 function loadText()
 {
- console.log('LOAD');
+    let saved = localStorage.getItem("saved");
+    textArea.value = saved;
 }
 
 function saveText()
 {
-    console.log(SAVE);
+
+    localStorage.setItem("saved",textArea.value);
     
 }
