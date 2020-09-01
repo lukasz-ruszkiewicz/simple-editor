8178a65a4b58f6c857e5e412226fa243334a9f97
lukasz-ruszkiewicz <karr190@wp.pl>
2020-09-01 10:57:26 +0200

    Customised ReadME

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,22 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀')
 
+let savedText = "";
+
+const textArea = document.querySelector('.editor-form__input');
+const buttonLoad = document.querySelector('.editor-form__button--load');
+const buttonSave = document.querySelector('.editor-form__button--save');
+
+buttonLoad.addEventListener('click', loadText);
+buttonSave.addEventListener('click', saveText);
+
+function loadText()
+{
+ console.log('LOAD');
+}
+
+function saveText()
+{
+    console.log(SAVE);
+    
+}
